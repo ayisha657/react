d26b53b19fb19ad01d29a3500e47f4cc26daa1f5
ayisha657 <ayishahanna657@gmail.com>
2024-01-18 17:02:15 -0800

    react simple app with the indroduction of components and JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Header from './components/Header';
 
 function App() {
+
+  const data ='description'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header data={data} />
+      <h1 style={{color:'red'}}>Hello world</h1>
+      <p>this is sample {data}</p>
+      
     </div>
   );
 }
